b6aba02db7becfd60851a7aff94bbbd63d13c282
Natália S. Gomes <nataliandsg32@gmail.com>
2025-08-07 13:00:08 -0300

    refactor: implement automatic creation of exchange, queue, and binding in RabbitMQ

--- certificates-data-upload/src/main/java/com/nataliatsi/certificatesdataupload/api/config/RabbitMQConfig.java
+++ certificates-data-upload/src/main/java/com/nataliatsi/certificatesdataupload/api/config/RabbitMQConfig.java
@@ -1,13 +1,41 @@
 package com.nataliatsi.certificatesdataupload.api.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
+
+    @Value("${app.rabbitmq.exchange}")
+    private String exchangeName;
+
+    @Value("${app.rabbitmq.routing-key}")
+    private String routingKey;
+
+    @Bean
+    public DirectExchange directExchange() {
+        return new DirectExchange(exchangeName, true, false);
+    }
+
+    @Bean
+    public Queue participantQueue() {
+        return new Queue("certificates.participant.queue", true);
+    }
+
+    @Bean
+    public Binding binding(Queue participantQueue, DirectExchange directExchange) {
+        return BindingBuilder.bind(participantQueue)
+                .to(directExchange)
+                .with(routingKey);
+    }
 
     @Bean
     public Jackson2JsonMessageConverter messageConverter() {
