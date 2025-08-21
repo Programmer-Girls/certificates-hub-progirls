a59da762062bed7fa9ea588a16af2c110b633b91
Natália S. Gomes <nataliandsg32@gmail.com>
2025-08-21 14:31:15 -0300

    refactor: add date field to EventDTO

--- certificates-data-upload/src/main/java/com/nataliatsi/certificatesdataupload/api/dto/EventDTO.java
+++ certificates-data-upload/src/main/java/com/nataliatsi/certificatesdataupload/api/dto/EventDTO.java
@@ -1,10 +1,13 @@
 package com.nataliatsi.certificatesdataupload.api.dto;
 
+import java.time.LocalDate;
+
 public record EventDTO(
         String name,
         String description,
         int workload,
         String speaker,
-        boolean remote
+        boolean remote,
+        LocalDate date
 ) {
 }
