9aee1eb1f0d7dc6148bc603f10667de94f7c73d9
Natália S. Gomes <nataliandsg32@gmail.com>
2025-08-22 14:50:41 -0300

    Updated RabbitMQ configuration; removed unnecessary methods for cleaner setup

--- certificates-generator/src/main/java/com/nataliatsi/certificatesgenerator/infrastructure/config/RabbitMQConfig.java
+++ certificates-generator/src/main/java/com/nataliatsi/certificatesgenerator/infrastructure/config/RabbitMQConfig.java
@@ -2,17 +2,17 @@ package com.nataliatsi.certificatesgenerator.infrastructure.config;
 
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
-
     @Bean
     public Jackson2JsonMessageConverter messageConverter() {
         return new Jackson2JsonMessageConverter();
