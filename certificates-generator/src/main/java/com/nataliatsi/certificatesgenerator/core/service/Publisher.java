6b7b6a772d829a3b9bba19317245b14de967ed4a
Natália S. Gomes <nataliandsg32@gmail.com>
2025-08-22 14:52:49 -0300

    feat: implement publisher component to publish certificates to queue

---
+++ certificates-generator/src/main/java/com/nataliatsi/certificatesgenerator/core/service/Publisher.java
@@ -0,0 +1,27 @@
+package com.nataliatsi.certificatesgenerator.core.service;
+
+import com.nataliatsi.certificatesgenerator.api.dto.CertificateMessageDTO;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Publisher {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    @Value("${app.rabbitmq.exchange}")
+    private String exchange;
+
+    @Value("${app.rabbitmq.routing-key}")
+    private String certificateRoutingKey;
+
+    public Publisher(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void publish(CertificateMessageDTO message) {
+        rabbitTemplate.convertAndSend(exchange, certificateRoutingKey, message);
+    }
+
+}
